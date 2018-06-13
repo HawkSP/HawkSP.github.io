fca2da4e1dfb04cdfd900a675ffd3c0749a5fa0b
Elliott <elliott.mitchell10@gmail.com>
2018-06-13 14:41:55 +0100

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,9 +1,16 @@
 <?php
-echo "<h2>Your Input:</h2>";
-echo $Name;
-echo "<br>";
-echo $Email;
-echo "<br>";
-echo $Message;
-echo "<br>";
-?>
+ 
+if ( isset( $_REQUEST ) && !empty( $_REQUEST ) ) {
+ if (
+ isset( $_REQUEST['Name'], $_REQUEST['Email'], $_REQUEST['Message'] ) &&
+  !empty( $_REQUEST['Name'] ) &&
+  !empty( $_REQUEST['Email'] )
+ ) {
+  $message = wordwrap( $_REQUEST['Message'], 70 );
+  $to = $_REQUEST['Name'] . '@' . $_REQUEST['Email'];
+  $result = @mail( $to, '', $message );
+  print 'Message was sent to ' . $to;
+ } else {
+  print 'Not all information was submitted.';
+ }
+}
